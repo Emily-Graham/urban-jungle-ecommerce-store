637935722e93a18fe5c6101ab218e53239089087
HowLargeTheTeeth <graham.emily15@gmail.com>
2021-10-26 18:50:58 +1100

    updated router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import firestore from "./services/firestore";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router >
+      <Switch>
+          <Route exact path="/">
+              <Home />
+          </Route>
+          <Route exact path="/:id">
+              <Product />
+          </Route>
+          <Route exact path="/cart">
+              <Cart />
+          </Route>
+      </Switch>
+    </Router>
   );
 }
 
